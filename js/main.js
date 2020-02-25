9eaae159007767f39ae8794c52579f9f4daf4674
Yukiko <yukikogimi@gmail.com>
2020-02-26 00:02:38 +1100

    updated

--- js/main.js
+++ js/main.js
@@ -1,102 +1,112 @@
 $(document).ready(function() {
-  const array = ["", "", "", "", "", "", "", "", ""];
-  let count = 0;
+  let array = ["", "", "", "", "", "", "", "", ""];
   let currentPlayer = true;
-     //true is X, false is O/// or set x;
+  let reset = false;
 
-     const checkWinner = function(){
-       if (count >= 3) {
 
-           if ( array[0] === "x" && array[1] === "x" && array[2] === "x" || array[0] === "o" && array[1] === "o" && array[2] === "o" ){
-             console.log("game is finished !!!");
+  const decidePlayer = function() {
+    if (Math.random() > 0.5) {
+      currentPlayer = true;
+    } else {
+      currentPlayer = false;
+    }
+  };
 
-           } else if (array[3] === "x" && array[4] === "x" && array[5] === "x" || array[3] === "o" && array[4] === "o" && array[5] === "o"){
-           console.log("game is finished !!!");
+  const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8]
+  ];
 
-          } else if (array[6] === "x" && array[7] === "x" && array[8] === "x" || array[6] === "o" && array[7] === "o" && array[8] === "o") {
-             console.log("game is finished !!!");
-
-           } else if (array[0] === "x" && array[3] === "x" && array[6] === "x" || array[0] === "o" && array[3] === "o" && array[6] === "o") {
-             console.log("game is finished !!!");
-
-           } else if (array[1] === "x" && array[4] === "x" && array[7] === "x" || array[1] === "o" && array[4] === "o" && array[7] === "o"){
-             console.log("game is finished !!!");
-
-           } else if (array[2] === "x" && array[5] === "x" && array[8] === "x" || array[2] === "o" && array[5] === "o" && array[8] === "o") {
-             console.log("game is finished !!!");
-
-           } else if (array[0] === "x" && array[4] === "x" && array[8] === "x" || array[0] === "o" && array[4] === "o" && array[8] === "o") {
-             console.log("game is finished !!!");
-
-           } else if (array[2] === "x" && array[4] === "x" && array[6] === "x" || array[2] === "o" && array[4] === "o" && array[6] === "o") {
-             console.log("game is finished !!!");
-           }
+  const checkWinner = function() {
+    for (let i = 0; i < winningConditions.length; i++) {
+      const current = array[winningConditions[i][0]];
+      if (current !== "") {
+        if (current === array[winningConditions[i][1]]) {
+          if (current === array[winningConditions[i][2]]) {
+            $("#turn").text("I win !!");
+            console.log("I win");
+          }
         }
-    };
+      }
+    }
+    if (!array.includes("")) {
+      $("#turn").text("The game is draw");
+      console.log("draw");
+    }
+  };
 
 
-  $("#turn").click( function(){
-    $("#turn").text("It is your trun")});
+///Game start////
+// 1 . if turn button is clicked, it will tell you who should play
+// 2.  randomly choose who should play first ex.. you or me ?
+// 3.  game starts
+// 4.
 
-
-  for (let i = 0; i < 9; i++) {
-    const button = $(`#${i}`);
-    button.click(function() {
-      console.log("click", i);
-      //checking if valid play
-      if (array[i] === "") {
-        //what marker will be displayed
-        if (currentPlayer === true) {
-          button.html('<img src="imges/macaron-red.png">');
-          array[i] = "x";
-          count += 1;
-          $("#turn").text("It is my trun");
-          checkWinner();
-
-
+  $("#turn").click(function() {
+    if (reset !== true){
+        decidePlayer();
+        if (currentPlayer) {
+          $("#turn").text("It is your turn");
+          gamePlay();
         } else {
-          button.html('<img src="imges/macaron-green.png">');
-          array[i] = "o";
-          count += 1;
-          $("#turn").text("It is your trun");
-          checkWinner();
-
+          $("#turn").text("It is my turn");
+          gamePlay();
         }
-        currentPlayer = !currentPlayer;
+  } else {
+        decidePlayer();
+        if (currentPlayer) {
+          $("#turn").text("It is your turn");
+          gamePlay();
+        } else {
+          $("#turn").text("It is my turn");
+          gamePlay();
 
       }
+    }
+  });
+
+
+  const gamePlay = function() {
+    for (let i = 0; i < array.length; i++) {
+      const button = $(`#${i}`);
+      button.click(function() {
+      console.log("click" + i);
+
+        //checking if valid play
+        if (array[i] === "") {
+
+          //what marker will be displayed
+          if (currentPlayer === true) {
+            button.html('<img src="imges/macaron-red.png">');
+            array[i] = "x";
+            $("#turn").text("It is my trun");
+            checkWinner();
+
+          } else {
+            button.html('<img src="imges/macaron-green.png">');
+            array[i] = "o";
+            $("#turn").text("It is your trun");
+            checkWinner();
+            }
+            currentPlayer = !currentPlayer
+          }
+        })
+      }
+    };
+
+  $("#reset").click(function() {
+    reset = true;
+    const button = $(".box");
+    button.html("");
+    $("#turn").text("♡Let's start♡");
+    
+
     });
-  }
+
 });
-
-
-//
-//
-// const findWinner = function(){
-//     if ( count >= 3 ) {
-//       for ( let i = 0 ; i < array.length; i ++){
-//         if ( array === (array[0] === array[1] === array[2]) || (array[3] === array[4] === array[5] ) || array[6] === array[7] === array[8] || array[0] === array[3] === array[6] || array[1] === array[4] === array[7] || array[2] === array[5] === array[8] || array[0] === array[4] === array[8] ||) {
-//         console.log("the Game is finished!!!")
-//       }
-//     } else if
-//
-//
-//
-
-
-
-
-//
-//
-//       ;
-//       ;
-//       ;
-//       ;
-//       ;
-//       ;
-//  {
-//
-//
-//   }
-// }
-//
