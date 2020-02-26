8c33ef4a910f60e565e37e0a380ca9b73c30c187
Yukiko <yukikogimi@gmail.com>
2020-02-26 23:54:18 +1100

    the last updated on wed

--- js/main.js
+++ js/main.js
@@ -5,25 +5,22 @@ let isGameReset = false;
 let choiceOfChange1;
 
 let color = {
-
- red: '<img src="imges/macaron-red.png">',
- green: '<img src="imges/macaron-green.png">',
- yellow: '<img src="imges/macaron-yellow.png">',
- purple: '<img src="imges/macaron-purple.png">',
- pink:'<img src="imges/macaron-pink.png">'
+  red: '<img src="imges/macaron-red.png">',
+  green: '<img src="imges/macaron-green.png">',
+  yellow: '<img src="imges/macaron-yellow.png">',
+  purple: '<img src="imges/macaron-purple.png">',
+  pink: '<img src="imges/macaron-pink.png">'
 };
 
 let buttonColor = {
-  purple:"#b19cd9",
-  yellow:"#fdfd96",
-  green:"#77dd77",
-  red:"#ff6961",
-  pink:"pink"
-
+  purple: "#b19cd9",
+  yellow: "#fdfd96",
+  green: "#77dd77",
+  red: "#ff6961",
+  pink: "pink"
 };
 
 $(document).ready(function() {
-
   const winningConditions = [
     [0, 1, 2],
     [3, 4, 5],
@@ -44,8 +41,11 @@ $(document).ready(function() {
             gameOver = true;
             // isGameReset = false;
 
-            // $(".box").html('<img src ="imges/cloud1.png">')
+            // $(
             $("#turn").text("You are winner !!");
+            $(".box")
+              .delay(800)
+              .html('<img src ="imges/cloud1.png">');
             $(".box").addClass("swing");
             console.log("I win");
           }
@@ -54,36 +54,33 @@ $(document).ready(function() {
     }
     if (!array.includes("")) {
       $("#turn").text("The game is draw");
-      $("#turn").css("background-color","#aec6cf")
-
-      // $(".box").html('<img src ="imges/cloud1.png">')
+      $("#turn").css("background-color", "#aec6cf");
+      $(".box")
+        .delay(800)
+        .html('<img src ="imges/cloud1.png">');
       console.log("draw");
     }
   };
 
-
-  const disenabled = function(){
-    if ( !gameOver && !isGameReset ){
-      $('#turn').prop('disabled', true)
+  const disenabled = function() {
+    if (!gameOver && !isGameReset) {
+      $("#turn").prop("disabled", true);
     } else {
-      $('#turn').prop('disabled', false)
+      $("#turn").prop("disabled", false);
     }
-  }
-
+  };
 
   $("#turn").click(function() {
-    if ( choiceOfChange1 === undefined ){
+    if (choiceOfChange1 === undefined) {
       return;
-    };
+    }
 
     $("#turn").text("It is your turn");
 
     isGameReset = false;
     disenabled();
 
-
     $(".box").on("click", function() {
-
       if (!gameOver && !isGameReset) {
         const i = this.id;
         if (array[i] === "") {
@@ -91,12 +88,10 @@ $(document).ready(function() {
             $(`#${i}`).html(choiceOfChange1);
             array[i] = "x";
             $("#turn").text("It is your turn");
-            $("#turn").css("background-color",choiceOfBackground)
-
+            $("#turn").css("background-color", choiceOfBackground);
           } else {
             $(`#${i}`).html(color.pink);
-            $("#turn").css("background-color",buttonColor.pink)
-
+            $("#turn").css("background-color", buttonColor.pink);
 
             array[i] = "o";
             $("#turn").text("It is my trun");
@@ -115,24 +110,21 @@ $(document).ready(function() {
     const button = $(".box");
     button.html("");
     $("#turn").text("♡Let's start♡");
-    $("#turn").css("background-color","pink")
+    $("#turn").css("background-color", "pink");
     array = ["", "", "", "", "", "", "", "", ""];
     disenabled();
-
   });
 
+  $(".choice").on("click", function() {
+    let colorChoice = $(this).attr("id");
+    choiceOfChange1 = color[colorChoice];
+    choiceOfBackground = buttonColor[colorChoice];
+    $("#turn").css("background-color", choiceOfBackground);
+  });
 
-$(".choice").on('click', function(){
-  let colorChoice = $(this).attr('id')
-  choiceOfChange1 = color[colorChoice]
-  choiceOfBackground = buttonColor[colorChoice]
-  $("#turn").css("background-color",choiceOfBackground)
-})
-
-
-
-
-
-
-
+  // const chooseYourColor = function(){
+  //   if ( Math.random() > 0.5){
+  //     let color
+  //   }
+  // }
 });
